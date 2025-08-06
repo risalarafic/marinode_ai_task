15905ef7748b7e6d5f065fcc4f397c5fee6c81b3
risalarafic <thathurisala@gmail.com>
2025-08-06 07:11:52 +0100

    Draggable Images

--- lib/view/dragable_images.dart
+++ lib/view/dragable_images.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DragableImages extends StatefulWidget {
-  const DragableImages({super.key});
+class Draggableimages extends StatefulWidget {
+  const Draggableimages({super.key});
 
   @override
-  State<DragableImages> createState() => _DragableImagesState();
+  State<Draggableimages> createState() => _DraggableimagesState();
 }
 
-class _DragableImagesState extends State<DragableImages> with SingleTickerProviderStateMixin {
+class _DraggableimagesState extends State<Draggableimages> with SingleTickerProviderStateMixin{
+
   List<String> images=[
     'assets/images/image1.jpg',
     'assets/images/image2.jpg',
@@ -26,41 +27,45 @@ class _DragableImagesState extends State<DragableImages> with SingleTickerProvid
     double screenwidth=MediaQuery.of(context).size.width;
     double itemsize=screenwidth/3;
     double hieght=itemsize*3;
+
+
     return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            width: screenwidth,
-            height: hieght,
-            child: Stack(
-                children:
-                List.generate(9, (index){
-                  int row = index ~/ 3;
-                  int col = index % 3;
-                  return AnimatedPositioned(
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.easeInOut,
-                    left: col * itemsize,
-                    top: row * itemsize,
-                    width: itemsize,
-                    height: itemsize,
-                    child: buildDragTarget(index, itemsize),
-                  );
-                })
+        appBar: AppBar(
+          title: Center(child: Text("Draggable Images",style: TextStyle(color: Colors.white),)),
+          backgroundColor: Colors.blue[200],
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: screenwidth,
+              height: hieght,
+              child: Stack(
+                  children:
+                  List.generate(9, (index){
+                    int row = index ~/ 3;
+                    int col = index % 3;
+                    return AnimatedPositioned(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      left: col * itemsize,
+                      top: row * itemsize,
+                      width: itemsize,
+                      height: itemsize,
+                      child: buildDragTarget(index, itemsize),
+                    );
+                  })
+              ),
             ),
           ),
-        ),
-      ),
+        )
     );
   }
 
   Widget buildDragTarget(int index, double size) {
     return DragTarget<int>(
-      onWillAcceptWithDetails: (details) => details.data != index,
-      onAcceptWithDetails: (details) {
+      onWillAccept: (fromIndex) => fromIndex != index,
+      onAccept: (fromIndex) {
         setState(() {
-          final fromIndex = details.data;
           final temp = images[fromIndex];
           images[fromIndex] = images[index];
           images[index] = temp;
@@ -72,21 +77,12 @@ class _DragableImagesState extends State<DragableImages> with SingleTickerProvid
     );
   }
 
-
   Widget buildDraggableItem(int index, double size) {
     return Draggable<int>(
       data: index,
-      onDragStarted: () {
-        setState((() {
-          draggingindex = index;
-        }));
-      },
-      onDraggableCanceled: (_, __) => setState(() {
-        draggingindex = null;
-      }),
-      onDragEnd: (_) => setState(() {
-        draggingindex = null;
-      }),
+      onDragStarted: () => setState(() => draggingindex = index),
+      onDraggableCanceled: (_, __) => setState(() => draggingindex = null),
+      onDragEnd: (_) => setState(() => draggingindex = null),
       feedback: Material(
         color: Colors.transparent,
         child: Container(
@@ -103,5 +99,7 @@ class _DragableImagesState extends State<DragableImages> with SingleTickerProvid
       ),
     );
   }
+
+
 
 }
